596e96e6bdd9da0cbc94301dceb2fa94a88a2257
Salvatore <salvatore@rezo.net>
2020-03-11 01:27:24 +0100

    [Salvatore] [source:lang/ geol_balades] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ geol_balades] Mise a jour du bilan depuis https://trad.spip.net

--- lang/geol_balades_fr.php
+++ lang/geol_balades_fr.php
@@ -1,20 +1,26 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite_balades.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'bouton_balader' => 'Générer',
+	// B
+	'bouton_balader' => 'Générer',
 
-'editer_balade' => 'Éditer une balade',
-'explication_album_balader' => 'Générer une balade à partir du contenu de cette collection.',
-'explication_importer' => 'Importer un fichier (KML ou GPX)',
-'explication_tracer' => 'Tracer le trajet',
-'explication_tracer_balade' => 'Importez un fichier (KML ou GPX) ou cliquez sur la carte pour commencer à tracer votre balade.',
+	// E
+	'editer_balade' => 'Éditer une balade',
+	'explication_album_balader' => 'Générer une balade à partir du contenu de cette collection.',
+	'explication_importer' => 'Importer un fichier (KML ou GPX)',
+	'explication_tracer' => 'Tracer le trajet',
+	'explication_tracer_balade' => 'Importez un fichier (KML ou GPX) ou cliquez sur la carte pour commencer à tracer votre balade.',
 
-'legend_ordonner' => 'Ordonner les médias',
-'legend_tracer' => 'Éditer le trajet',
+	// L
+	'legend_ordonner' => 'Ordonner les médias',
+	'legend_tracer' => 'Éditer le trajet',
 
-'modifier_balade' => 'Modifier la balade',
-
+	// M
+	'modifier_balade' => 'Modifier la balade'
 );
