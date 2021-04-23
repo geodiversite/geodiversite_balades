f0e14a383a5edecaa7f895090b3a3e3cf2cd9905
brunob <bruno@eliaz.fr>
2021-04-23 15:53:06 +0200

    charger la css du module en passant par le pipeline insert_head_css
    + rangement de la css dans un dossier dédié

--- geol_balades_pipelines.php
+++ geol_balades_pipelines.php
@@ -3,6 +3,17 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
+ * Insertion des css du plugin dans les pages publiques
+ *
+ * @param $flux
+ * @return mixed
+ */
+function geol_balades_insert_head_css($flux) {
+	$flux .= "\n".'<link rel="stylesheet" href="'. find_in_path('css/geol_balades.css') .'" />';
+	return $flux;
+}
+
+/**
  * Insertion dans le pipeline formulaire_charger (SPIP)
  * 
  * Surcharge du formulaire de gis dans l'espace public
