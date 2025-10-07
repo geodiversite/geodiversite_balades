97cac7c5b7d1713d068d112188b7411d81ede515
brunob <bruno@eliaz.fr>
2025-10-07 15:40:50 +0200

    style: CS

--- geol_balades_pipelines.php
+++ geol_balades_pipelines.php
@@ -7,7 +7,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 /**
  * Insertion des css du plugin dans les pages publiques
  *
- * @param $flux
  * @return mixed
  */
 function geol_balades_insert_head_css($flux) {
@@ -21,7 +20,7 @@ function geol_balades_insert_head_css($flux) {
  * Surcharge du formulaire de gis dans l'espace public
  *
  * @param array $flux
- * @return array $flux
+ * @return array
  */
 function geol_balades_formulaire_charger($flux) {
 	if (($flux['args']['form'] == 'editer_gis') and !test_espace_prive()) {
